27fe2c48876325669663249927a0c6df1ab72e1a
farhanang <75283346+farhanangwa12@users.noreply.github.com>
2025-02-04 16:53:53 +0700

    menambahkan foreign key dengan jenis berbeda

--- migrations/1738465040540_add-owner-column-playlists-table.js
+++ migrations/1738465040540_add-owner-column-playlists-table.js
@@ -8,22 +8,30 @@
  * @returns {Promise<void> | void}
  */
 exports.up = (pgm) => {
+  // Menambahkan kolom 'owner' ke tabel 'playlists'
   pgm.addColumn('playlists', {
     owner: {
       type: 'VARCHAR(50)',
       notNull: true,
-      references: 'users(id)',
-      onUpdate: 'CASCADE',
-      onDelete: 'CASCADE',
-    }
+    },
   });
+
+  // Menambahkan constraint foreign key untuk kolom 'owner'
+  pgm.addConstraint(
+    'playlists',
+    'fk_playlists_owner',
+    'FOREIGN KEY(owner) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE'
+  );
 };
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
  */
 exports.down = (pgm) => {
+  // Menghapus constraint foreign key terlebih dahulu
+  pgm.dropConstraint('playlists', 'fk_playlists_owner');
+
+  // Menghapus kolom 'owner'
   pgm.dropColumn('playlists', 'owner');
 };
+
